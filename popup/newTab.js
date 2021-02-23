1a6dc52213ab9d815e7e34110fd63a88e357da5a
extwooh <ext-winfred.selwyn.ooh@solenovo.fi>
2021-02-23 22:11:53 +0800

    Cleanup and added comments.

--- popup/newTab.js
+++ popup/newTab.js
@@ -1,45 +1,45 @@
+// this function creates a new tab in the user's browser and displays items in their database
 document.addEventListener("DOMContentLoaded", async function () {
+  // entries array for storing items from browser's localforage
   const entries = [];
-  const keys = await localforage.keys();
+  const keys = await localforage.keys(); // asynchronously retrieve all keys
   
   for (let i in keys) {
     const rawEntry = await localforage.getItem(keys[i]);
+    // parse error free entries
     try {
       const parsed = JSON.parse(rawEntry);
+      // push entries which are not images to entries array
       if (parsed.id && !parsed.address.includes("chrome://")) {
         entries.push(parsed);
       }
     } catch(error) {
-      // do nothing
+      // if error or image, do nothing
       ;
     }
   }
 
   if (entries.length !== 0) {
     for (let i = 0; i < entries.length; i += 1) {
+      // create a new <a> tag
       const newAnchorTag = document.createElement("a");
       let newContent;
+
       if (entries[i].readingTime) {
+        // display total reading time if available
         newContent = document.createTextNode(`Link: ${entries[i].address.split("//")[1].split("/")[0]}, Reading Time: ${entries[i].readingTime} minutes`);
       } else {
         newContent = document.createTextNode(`Link: ${entries[i].address.split("//")[1].split("/")[0]}`);
       }
+
       newAnchorTag.appendChild(newContent);
-
-      // if (entries[i].readingTime) {
-      //   console.log("TEST", entries[i].address.split("//")[1])
-      //   newAnchorTag.title = `${entries[i].address.split("//")[1]}, Reading Time: ${entries[i].readingTime} minutes`;
-      // } else {
-      //   newAnchorTag.title = entries[i].address.split("//")[1];
-      // }
-
       newAnchorTag.title = entries[i].address.split("//")[1];
       newAnchorTag.href = entries[i].address;
       newAnchorTag.target = "_blank"; // open link in new tab
       newAnchorTag.style.display = "block";
 
+      // append new <a> tag to body of web page
       document.body.appendChild(newAnchorTag);
-
     }
   }
 });
